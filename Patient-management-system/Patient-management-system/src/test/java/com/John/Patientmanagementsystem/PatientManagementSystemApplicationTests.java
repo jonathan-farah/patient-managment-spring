ab5e838d27cceab663fac2ee2e7c2d0254f35f20
Jonathan <jonathanfarah05@gmail.com>
2022-05-30 14:57:35 -0400

    added spring boot base

---
+++ Patient-management-system/Patient-management-system/src/test/java/com/John/Patientmanagementsystem/PatientManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.John.Patientmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
