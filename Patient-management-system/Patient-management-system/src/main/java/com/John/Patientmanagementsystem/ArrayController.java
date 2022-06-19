953812c3cc22d83831ed24f562bef4f28022914e
Jonathan <jonathanfarah05@gmail.com>
2022-06-19 16:21:34 -0400

    we started adding endpoint for number arrays

---
+++ Patient-management-system/Patient-management-system/src/main/java/com/John/Patientmanagementsystem/ArrayController.java
@@ -0,0 +1,24 @@
+package com.John.Patientmanagementsystem;
+
+import java.util.ArrayList;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ArrayController {
+	private ArrayList<Integer> array = new ArrayList<Integer>();
+	@GetMapping("/addnum")
+	public String addNumber(@RequestParam(name = "number", required = false,
+	defaultValue = "0")int n) {
+		array.add(n);
+		String returnVal = "";
+		returnVal += "succesfully added #: " + n +"\n array is "+ array;
+		return returnVal;
+	}
+	@GetMapping("/eraseArray")
+		public String eraser() {
+			array.clear();
+			return "Array has been cleared"+ array;
+		}
+	}
