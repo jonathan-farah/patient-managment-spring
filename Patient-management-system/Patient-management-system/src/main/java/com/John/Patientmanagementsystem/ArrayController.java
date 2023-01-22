e545394ece7e96ca00a5a1e19adde44d1ff934ef
jonathan-farah <jonathanfarah05@gmail.com>
2023-01-22 16:15:12 -0500

    added patient info form

--- Patient-management-system/Patient-management-system/src/main/java/com/John/Patientmanagementsystem/ArrayController.java
+++ Patient-management-system/Patient-management-system/src/main/java/com/John/Patientmanagementsystem/ArrayController.java
@@ -8,6 +8,8 @@ import org.apache.catalina.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,50 +19,22 @@ public class ArrayController {
 	private ArrayList<Patient> array = new ArrayList<Patient>();
 	@GetMapping("/index")
 	public String welcome (Model model) {
+		//creating new patient object for model
 		Patient patient = new Patient();
+		//adding new patient object to model
 		model.addAttribute("patient",patient);
+		//adding PatientArray to model
 		model.addAttribute("PatientTable", this.array);
         return "Homepage";
 	}
-	@GetMapping("/addPatient")
-	public String addPatient(@RequestParam(name = "age", required = true,
-	defaultValue = "0")String n, @RequestParam(name = "name", required = true)  
-	String name,@RequestParam(name = "StartDate", required = true)String sd,
-	@RequestParam(name = "EndDate", required = true )String ed) {
-		//check for valid age
-		int ageNumber;
-		 Date date1;
-		 Date date2;
-		try {
-			ageNumber = Integer.parseInt(n);
-		}
-		catch (Exception e) {
-			return "InvalidInputAge";
-		}
-		if(ageNumber <= 0) {
-			return "InvalidInputAge";
-		}
-		SimpleDateFormat sdfrmt = new SimpleDateFormat("MM/dd/yyyy");
-		sdfrmt.setLenient(false);
-		try {
-		     date1 = sdfrmt.parse(sd);
-		}
-		catch(Exception e) {
-			return "InvalidInputDate";
-		}
-		try {
-		     date2 = sdfrmt.parse(ed);
-		}
-		catch(Exception e) {
-			return "InvalidInputDate";
-		}
-		Patient p = new Patient(ageNumber,name,date1,date2);
-		array.add(p);
+	@PostMapping("/index")
+	public String acceptSubmit(@ModelAttribute("patient") Patient patient) {
+		array.add(patient);
 		return "addPatientSuccess";
 	}
 	@GetMapping("/eraseArray")
 	public String eraser() {
 		array.clear();
-		return "Array has been cleared"+ array;
+		return "clearPatientRecords";
 	}
 }
